7686973d88a6e678ee46974a613e32690bc89110
Pierre MOUDENS <pierre.moudens@cgi.com>
2019-11-28 14:47:55 +0100

    First Commit

---
+++ src/Controller/Login/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller\Login;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\Login;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login")
+     */
+    public function login()
+    {
+        if (isset($_GET['code'])) {
+            $code = $_GET['code'];
+
+            $login = new Login();
+            $token = $login->getToken($code);
+
+            echo 'token = ';
+            print_r($token);
+
+            return new Response('');
+        }
+    }
+
+    /**
+     * @Route("/refresh")
+     */
+    public function refreshToken()
+    {
+        if (isset($_COOKIE['refresh_token'])) {
+            $refresh = $_COOKIE['refresh_token'];
+            $login = new Login();
+            $refresh_token = $login->refresh($refresh);
+        }
+    }
+}
---
+++ src/Services/Login.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Services;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class Login 
+{
+	const URL = "https://www.bungie.net/Platform/App/OAuth/token/";
+	const AUTH = 'Basic ';
+	const CLIENT_ID = "31249";
+	const SECRET_TOKEN = "9CT6JX8cLr9qwSgXe3EWE1Ea9hlG1PPvSKj0Xdv6XhE";
+
+    public function getToken($code)
+    {
+		$client = HttpClient::create();
+		$response = $client->request('POST', self::URL, [
+			'headers' => [
+				'Content-Type' => 'application/x-www-form-urlencoded',
+				'Authorization' => self::AUTH . base64_encode(self::CLIENT_ID . ":" . self::SECRET_TOKEN)
+			],
+			'body' => [
+				'grant_type' => 'authorization_code',
+				'code' => $code
+			],
+		]);
+
+        return json_decode($response->getContent());
+    }
+}
