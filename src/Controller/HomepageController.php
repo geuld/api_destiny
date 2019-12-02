5b09ee8b2cd7bcd68370e650f29135c4843357c0
MAINGUY, Valentin <valentin.mainguy@cgi.com>
2019-12-02 14:40:12 +0100

    assets ok

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -15,7 +15,7 @@ class HomepageController extends AbstractController
      */
     public function index()
     {
-        return $this->render('base.html.twig');
+        return $this->render('homepage.html.twig');
     }
 
     /**
