09ed22297340ad4678ccf2ad83c3d822bbe506f4
Pierre MOUDENS <pierre.moudens@cgi.com>
2019-12-03 16:19:58 +0100

    update

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,35 +5,48 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Services\SearchPlayer;
+use App\Services\Login;
 use Exception;
 use Psr\Log\LoggerInterface;
 
 class HomepageController extends AbstractController
 {
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->login = new Login($logger);
+        $this->search = new SearchPlayer($logger);
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index(LoggerInterface $logger)
+    public function index()
     {
-        $logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
-        return $this->render('homepage.html.twig');
+        $this->logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
+        try {
+            $this->login->ifSession();
+            return $this->render('homepage.html.twig');
+        } catch (Exception $e) {
+            $this->logger->error(__CLASS__ . '->' . __FUNCTION__ . ' => ' . $e->getMessage());
+            return $this->redirectToRoute('homepage', ['error' => 1]);
+        }
+        
     }
 
     /**
      * @Route("/player/{plateforme}/{gamertag}", name="search_player")
      */
-    public function searchPlayer(Int $plateforme, String $gamertag, LoggerInterface $logger)
+    public function searchPlayer(Int $plateforme, String $gamertag)
     {
-        $logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
-
-        $search = new SearchPlayer();
+        $this->logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
 
         try {
-            $id = $search->getPlayerId($plateforme, $gamertag);
-            $player = $search->getPlayerById($id, $plateforme);
+            $id = $this->search->getPlayerId($plateforme, $gamertag);
+            $player = $this->search->getPlayerById($id, $plateforme);
             return $this->render('playerInfos.html.twig', ['player' => $player]);
         } catch (Exception $e) {
-            $logger->error(__CLASS__ . '->' . __FUNCTION__ . ' => ' . $e->getMessage());
+            $this->logger->error(__CLASS__ . '->' . __FUNCTION__ . ' => ' . $e->getMessage());
             return $this->redirectToRoute('homepage', ['error' => 1]);
         }
     }
--- src/Services/SearchPlayer.php
+++ src/Services/SearchPlayer.php
@@ -3,21 +3,23 @@ namespace App\Services;
 
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Dotenv\Dotenv;
+use Psr\Log\LoggerInterface;
 
 class SearchPlayer
 {
     const URL_GET_PLAYER_ID = "Destiny2/SearchDestinyPlayer/";
 
-    public function __construct()
+    public function __construct(LoggerInterface $logger)
     {
-        $this->_dotenv = new Dotenv();
-        $this->_dotenv->load(__DIR__.'\..\../.env');
+        $this->dotenv = new Dotenv();
+        $this->dotenv->load(__DIR__.'\..\../.env');
+        $this->logger = $logger;
     }
 
     public function getPlayerId(Int $plateforme, String $gamertag)
     {
+        $this->logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
         $url = $_ENV['URL_BUNGIE'] . self::URL_GET_PLAYER_ID . $plateforme . '/' . $gamertag;
-
         $client = HttpClient::create();
         $response = $client->request('POST', $url, [
             'headers' => [
@@ -30,8 +32,8 @@ class SearchPlayer
 
     public function getPlayerById(String $id, Int $plateforme)
     {
+        $this->logger->info(__CLASS__ . '->' . __FUNCTION__ . ' DEBUT');
         $url = $_ENV['URL_BUNGIE'] . 'Destiny2/' . $plateforme . '/Profile/' . $id . '?Components=' . $_ENV['PLAYER_COMPONENTS'];
-
         $client = HttpClient::create();
 		$response = $client->request('POST', $url, [
 			'headers' => [
